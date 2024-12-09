593922eb63cfca86b2a7b63c6c557dcaff25bf3f
civilix <i@civilux.cc>
2024-12-09 13:35:35 +0900

    Update ClassificationAnalysis.jsx

--- frontend/src/components/ClassificationAnalysis.jsx
+++ frontend/src/components/ClassificationAnalysis.jsx
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 const ClassificationAnalysis = ({ data }) => {
   return (
     <Card title="分类分析">
-      <p>分类分析功能开发中...</p>
+      <p>In progress...</p>
     </Card>
   );
 };
