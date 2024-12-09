4d7b6a0aeab5192e572174c7fe8edfcf61849b97
civilix <i@civilux.cc>
2024-12-09 10:55:53 +0900

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
         justifyContent: 'space-between',
         position: 'sticky',
         top: 0,
-        zIndex: 1,
+        zIndex: 1000,
       }}
     >
       <div style={{ 
