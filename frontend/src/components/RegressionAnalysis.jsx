f279257a3a9b9a06205d3be14b19932c30ee66e6
civilix <i@civilux.cc>
2024-12-09 13:35:47 +0900

    Update RegressionAnalysis.jsx

--- frontend/src/components/RegressionAnalysis.jsx
+++ frontend/src/components/RegressionAnalysis.jsx
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 const RegressionAnalysis = ({ data }) => {
   return (
     <Card title="回归分析">
-      <p>回归分析功能开发中...</p>
+      <p>In progress...</p>
     </Card>
   );
 };
