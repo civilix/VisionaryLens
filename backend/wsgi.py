9d19e09db3ec154bae430a669a6a8cb589b3f25d
civilix <i@civilux.cc>
2024-12-12 15:15:51 +0900

    feat: add separate configurations for development and production environments"

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -123,9 +123,9 @@ def get_insights():
         )
     })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8080)
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
